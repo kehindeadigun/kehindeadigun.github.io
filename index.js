53579914455df74952e3f919dcb2123042136dc1
Kehinde Adigun <kehinde@getclue.com>
2026-03-09 01:00:36 +0100

    Update device shop checkout for form submission

--- index.js
+++ index.js
@@ -4,6 +4,17 @@ function handleSubmit (event) {
     console.log("Form submission prevented!");
 }
 
+function handleClose({target}) {
+    let targetItem = target.closest(".item");
+    targetItem.style.display = "none";
+    console.log("Close event fired");
+}
+
+const closeButtons = document.querySelectorAll(".close-button");
+closeButtons.forEach(ele => {
+    ele.addEventListener("click", handleClose);
+})
+
 const forms = document.querySelectorAll("form");
 forms.forEach(ele => {
     ele.addEventListener("submit", handleSubmit);
